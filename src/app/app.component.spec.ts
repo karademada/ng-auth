bdd2973c696d74ad81486f0a1683964f30976c08
lazantsy <christophe.lazantsy@gmail.com>
2020-05-25 15:43:45 +0200

    add ci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'authentication'`, () => {
+  it(`should have as title 'Tour of Heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('authentication');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('authentication app is running!');
+    expect(compiled.querySelector('h2').textContent).toContain('Tour of Heroes app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HereosComponent } from './hereos/hereos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HereosComponent
   ],
   imports: [
     BrowserModule,
